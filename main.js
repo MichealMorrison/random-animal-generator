651565c854fc72aa69d8b10c3efeabaa695ddffb
Mikey Morrison <morrisonhunting@outlook.com>
2022-09-14 17:04:38 -0600

    Added habitat array and pnp array

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const earthSpecies = ['lion', 'elephant',
-'cheeta','rhinoceros','leopard', 'gorilla','penguins', 'seals', 'albatross', 'monkey', 'panda', 'tiger', 'orangutan', 'leopard', 'cobra', 'kangaroo', 'koala','crocodile', 'dingo', 'platypus','lynx','reindeer', 'bison', 'bear','wold','moose', 'bobcat','liama', 'sloth', 'anaconda','jaguar'];
-
+//array to store earth species.
+const earthSpecies = ['lion', 'elephant','cheeta','rhinoceros','leopard', 'gorilla','penguins', 'seals', 'albatross', 'monkey', 'panda', 'tiger', 'orangutan', 'leopard', 'cobra', 'kangaroo', 'koala','crocodile', 'dingo', 'platypus','lynx','reindeer', 'bison', 'bear','wold','moose', 'bobcat','liama', 'sloth', 'anaconda','jaguar'];
+//array to store diet type
 const typeOfDiet = ['carnivore', 'herbivore', 'omnivore'];
+//array to store habitats
+const habitat = ['wetlands', 'tundra', 'savanna', 'tropical rainforest', 'desert', 'artic', 'grassland', 'mountains', 'coast', 'lake and ponds'];
+//array to store weather it is a prey or predator
+const pnp = ['prey', 'predator'];
+
