615cb7b3a80bf9c2196c0d6739cb38eb64cf6502
Mikey Morrison <morrisonhunting@outlook.com>
2022-09-14 16:48:35 -0600

    Added Arrays to program

---
+++ main.js
@@ -0,0 +1,4 @@
+const earthSpecies = ['lion', 'elephant',
+'cheeta','rhinoceros','leopard', 'gorilla','penguins', 'seals', 'albatross', 'monkey', 'panda', 'tiger', 'orangutan', 'leopard', 'cobra', 'kangaroo', 'koala','crocodile', 'dingo', 'platypus','lynx','reindeer', 'bison', 'bear','wold','moose', 'bobcat','liama', 'sloth', 'anaconda','jaguar'];
+
+const typeOfDiet = ['carnivore', 'herbivore', 'omnivore'];
